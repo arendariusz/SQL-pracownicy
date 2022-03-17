b7673f2a26bce11590946f3b1bb76e599a26f42c
arendariusz <d.arendarski92@gmail.com>
2022-03-17 19:23:48 +0100

    SQL-pracownicy

---
+++ src/main/java/com/example/sqlpracownicy/SqlPracownicyApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqlpracownicy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlPracownicyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlPracownicyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sqlpracownicy/SqlPracownicyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqlpracownicy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlPracownicyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
